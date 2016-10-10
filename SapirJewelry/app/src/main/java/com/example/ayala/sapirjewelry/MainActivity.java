82bc739ae8d827c743cb836e96dcd8894cd09178
Ayala-Bartal <ayalaw20@gmail.com>
2016-10-10 17:43:43 +0300

    Updating the layout

--- SapirJewelry/app/src/main/java/com/example/ayala/sapirjewelry/MainActivity.java
+++ SapirJewelry/app/src/main/java/com/example/ayala/sapirjewelry/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ayala.sapirjewelry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout shop = (LinearLayout)findViewById(R.id.new_colaction);
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        LinearLayout colaction = (LinearLayout)findViewById(R.id.new_colaction);
+        colaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        LinearLayout sales = (LinearLayout)findViewById(R.id.sales);
+        sales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        LinearLayout club = (LinearLayout)findViewById(R.id.club);
+        club.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageView home = (ImageView) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ImageView contact = (ImageView) findViewById(R.id.conact);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
